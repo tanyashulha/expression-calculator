b3eceedab9da587cb8c88c214504aa041cf89774
tanyashulha <tsulga6@gmail.com>
2019-09-30 21:57:27 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -4,8 +4,46 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    var chars = expr.split("");
+    var n = [], op = [], index = 0, oplast = true;
+
+    n[index] = "";
+
+    for (var c = 0; c < chars.length; c++) {
+
+        if (isNaN(parseInt(chars[c])) && chars[c] !== "." && !oplast) {
+            op[index] = chars[c];
+            index++;
+            n[index] = "";
+            oplast = true;
+        } else {
+            n[index] += chars[c];
+            oplast = false;
+        }
+    }
+
+    expr = parseFloat(n[0]);
+    for (var o = 0; o < op.length; o++) {
+        var num = parseFloat(n[o + 1]);
+        switch (op[o]) {
+            case "+":
+                expr = expr + num;
+                break;
+            case "-":
+                expr = expr - num;
+                break;
+            case "*":
+                expr = expr * num;
+                break;
+            case "/":
+                expr = expr / num;
+                break;
+        }
+    }
+
+    return expr;
 }
+
 
 module.exports = {
     expressionCalculator
