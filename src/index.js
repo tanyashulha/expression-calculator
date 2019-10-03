5b9ca328bb9aac852d83601c00c64f383a2fc2c4
Tatyana Shulha <tsulga6@gmail.com>
2019-10-03 10:29:14 +0300

    add solutin

--- src/index.js
+++ src/index.js
@@ -3,10 +3,32 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) {
-    // write your solution here
-}
-
+function expressionCalculator(exp) {
+    const sign = {
+      '+': (a, b) => { return parseFloat(a) + parseFloat(b) },
+      '-': (a, b) => { return parseFloat(a) - parseFloat(b) },
+      '*': (a, b) => { return parseFloat(a) * parseFloat(b) },
+      '/': (a, b) => { return parseFloat(a) / parseFloat(b) },
+    };
+    const signList = Object.keys(sign);
+  
+    let acc = 0;
+    let next = '';
+    let currSign = '+';
+  
+    for (let char of exp) {
+      if (signList.includes(char)) {
+        acc = sign[currSign](acc, next);
+        currSign = char;
+        next = '';
+      } else {
+        next += char;
+      }
+    }  
+  
+    return currSign === '+' ? acc + parseFloat(next) : acc - parseFloat(next);
+  }
+  
 module.exports = {
     expressionCalculator
 }
