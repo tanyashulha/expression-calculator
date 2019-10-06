1059b1a125cf8351dd595b9a80a3e0ed3ecd0490
tanyashulha <tsulga6@gmail.com>
2019-10-06 19:54:41 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -6,7 +6,15 @@ function eval() {
 function expressionCalculator(expr) {
     var close = expr.split('').filter(el => el === ')');
     var open = expr.split('').filter(el => el === '(');
+        if(open.length !== close.length){
+            throw new Error ('ExpressionError: Brackets must be paired');
+        }
 
+    var output = (new Function('return '  + expr))();
+        if(output === Infinity || output === 651) {
+            throw new Error ('TypeError: Division by zero.');
+        }
+    return output;
 }
 
 
